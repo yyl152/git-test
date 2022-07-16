5731dffe57658eb4bda065d1e8d954d58fa2c265
yyl <yyl@atguigu.com>
2022-07-16 16:20:40 +0800

    master commit2

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git111!");
         System.out.println("hello git222!");
         System.out.println("hello git333!");
+        System.out.println("master test!");
     }
 }
