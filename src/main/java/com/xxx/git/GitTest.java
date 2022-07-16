ba2a4c39c192854c9278f4ef667538d9d764dd1f
yyl <yyl@atguigu.com>
2022-07-16 16:17:44 +0800

    hot-fix commit1

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git111!");
         System.out.println("hello git222!");
+        System.out.println("hello git333!");
     }
 }
