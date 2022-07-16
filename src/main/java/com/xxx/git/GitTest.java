facb06e7b991d64abadeb7eea31d4a866ea11ca9
yyl <yyl@atguigu.com>
2022-07-16 16:07:48 +0800

    second commit

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xxx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git111!");
+        System.out.println("hello git222!");
     }
 }
