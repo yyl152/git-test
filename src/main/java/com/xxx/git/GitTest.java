b39e9facd68b1140a3474c85806662df7df7bdc0
yyl <yyl@atguigu.com>
2022-07-16 16:32:26 +0800

    push commit2

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git333!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
